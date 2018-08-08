32014f5235effb7f93db4374acf785aa4357b986
Harvey Zack <laozhaochaguan@gmail.com>
2018-08-08 11:42:37 +0800

    add css

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import Link from "next/link";
+import styled from "styled-components";
+import { DatePicker } from 'antd';
 
 const Title = styled.h1`
   color: red;
   font-size: 50px;
-`
+`;
 
-export default () => <div>
-    <Title>My page</Title>
-    <img src="/static/img/avatar.png" />
-</div>
+export default class extends React.Component {
+  static async getInitialProps({ req }) {
+    const userAgent = req ? req.headers["user-agent"] : navigator.userAgent;
+    return { userAgent };
+  }
+
+  render() {
+    return (
+      <div>
+        <DatePicker />
+        <Link href={{ pathname: '/about', query: { name: 'Zeit' } }}>
+          <a>here</a>
+        </Link>
+        <Title>My page</Title>
+        <img src="/static/img/avatar.png" />
+        Hello World {this.props.userAgent}
+      </div>
+    );
+  }
+}
